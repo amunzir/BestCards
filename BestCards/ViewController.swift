ada9a8cb98540fe8e338298621384ee2d4658aa7
Aamina Munzir <aaminamunzir123@gmail.com>
2019-07-30 15:24:13 -0500

    set up main storyboard

--- BestCards/ViewController.swift
+++ BestCards/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCardLabel: UILabel!
+    @IBOutlet weak var secondCardLabel: UILabel!
+    @IBOutlet weak var thirdCardLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func goBtnPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 

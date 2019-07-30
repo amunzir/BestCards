0fed831db028a4a5573af0995bf0caf8a8ea9f54
Aamina Munzir <aaminamunzir123@gmail.com>
2019-07-30 15:37:37 -0500

    set segmented controls

--- BestCards/ViewController.swift
+++ BestCards/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstCardLabel: UILabel!
     @IBOutlet weak var secondCardLabel: UILabel!
     @IBOutlet weak var thirdCardLabel: UILabel!
+    @IBOutlet weak var firstSelectedSC: UISegmentedControl!
+    @IBOutlet weak var secondSelectedSC: UISegmentedControl!
     
+    var firstAttribute = "Decision"
+    var secondAttribute = "Decision"
     
     
     override func viewDidLoad() {
@@ -21,8 +25,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func firstSCPressed(_ sender: UISegmentedControl)
+    {
+        switch firstSelectedSC.selectedSegmentIndex
+        {
+        case 0:
+            firstAttribute = "Decision"
+        case 1:
+            firstAttribute = "Creativity"
+        case 2:
+            firstAttribute = "Affinity"
+        case 3:
+            firstAttribute = "Execution"
+        default:
+            break
+        }
+    }
+    @IBAction func secondSCPressed(_ sender: UISegmentedControl)
+    {
+        switch secondSelectedSC.selectedSegmentIndex
+        {
+        case 0:
+            secondAttribute = "Decision"
+        case 1:
+            secondAttribute = "Creativity"
+        case 2:
+            secondAttribute = "Affinity"
+        case 3:
+            secondAttribute = "Execution"
+        default:
+            break
+        }
+    }
     
-    @IBAction func goBtnPressed(_ sender: UIButton) {
+    @IBAction func goBtnPressed(_ sender: UIButton)
+    {
+        print(firstAttribute + " " + secondAttribute)
     }
     
     
